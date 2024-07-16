57557024ab122f094c0c842e0cfb1784d5a4a3bb
Alejo Kim Uy <alejokimuy@gmail.com>
2024-07-16 08:01:59 -0600

    Added conditional rendering for city_name

--- src/components/city_name.jsx
+++ src/components/city_name.jsx
@@ -1,13 +1,17 @@
-function CityName() {
-	return (
-		<>
-			<div className="mb-2.5 w-full">
-				<div className="text-center text-white text-2xl">
-					CITY, COUNTRY
+function CityName({ address }) {
+	if (address) {
+		return (
+			<>
+				<div className="mb-2.5 w-full">
+					<div className="text-center text-white text-2xl md:text-4xl md:mt-3">
+						{address}
+					</div>
 				</div>
-			</div>
-		</>
-	);
+			</>
+		);
+	} else {
+		return null;
+	}
 }
 
 export default CityName;
