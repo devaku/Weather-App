d0c39deb9390173539c6b298d5162efbb122e589
Alejo Kim Uy <alejokimuy@gmail.com>
2024-07-18 07:43:34 -0600

    Changed animations to transitions

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import SearchSetting from './SearchSetting';
 import TemperatureSetting from './TemperatureSetting';
-import svgCross from '../assets/cross.svg';
+import SvgCross from './svgs/SvgCross';
 
 function Sidebar({ displaySidebar, setDisplaySidebar }) {
 	function handleOnClick(e) {
@@ -8,32 +8,29 @@ function Sidebar({ displaySidebar, setDisplaySidebar }) {
 		e.stopPropagation();
 	}
 
-	// function handleAnimationEnd(e) {
-	// 	if (e.animationName == 'slide-in') {
-	// 		setDisplaySidebar(false);
-	// 	} else {
-	// 		setDisplaySidebar(true);
-	// 	}
-	// }
-
 	return (
 		<>
 			<div
 				// onAnimationEnd={handleAnimationEnd}
 				className={
-					'absolute w-full bottom-2 h-full bg-gradient-to-b from-black ' +
-					(displaySidebar ? 'slide-in ' : 'slide-out')
+					'absolute w-full bottom-2 h-full bg-gradient-to-b from-black from-30% via-black via-10% to-transparent side-bar ' +
+					(displaySidebar ? 'slide-in-from-top ' : 'slide-out-to-top')
 				}>
-				<div className="py-5 w-5/12 mx-auto">
+				<div className="py-5 w-full md:w-5/12 mx-auto">
 					{/* Setting Titlebar */}
 					<div className="flex items-center">
 						{/* Burger Close */}
-						<div className="w-10 h-10" onClick={handleOnClick}>
-							<img
+						<div
+							className="w-10 h-10 ml-[5px] md:m-0"
+							onClick={handleOnClick}>
+							<div className="w-full h-full">
+								<SvgCross twColor="fill-white"></SvgCross>
+							</div>
+							{/* <img
 								className="invert w-full h-full"
 								src={svgCross}
 								alt="Close Settings"
-							/>
+							/> */}
 						</div>
 						{/* Text */}
 						<div className="text-white text-lg">Settings</div>
