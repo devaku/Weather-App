4d113d7480194fb5fa4996039470607d174f15c1
Alejo Kim Uy <alejokimuy@gmail.com>
2025-04-12 09:36:12 -0600

    Removed scratch

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import Scratch from './components/Scratch.jsx';
 import './css/output.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<App />
-		{/* <Scratch></Scratch> */}
 	</React.StrictMode>
 );
